71b8334abfea625ae64eb50f81317ce9fb66c1d2
Piotr Kałuziński <piotr.kaluzinski@student.wat.edu.pl>
2022-05-04 18:23:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Piekarnia Pieczonka</p>
+      <Switch>
+        <Route path='/hello'>
+          <p>Hello world</p>
+        </Route>
+        <Route path='/about'>
+          <p>About us</p>
+        </Route>
+      </Switch>
     </div>
   );
 }
