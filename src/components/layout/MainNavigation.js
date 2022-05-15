d94ad8e3788ab3a10430730a02f6954c1bba2fbd
beatadz <beata.dziewulska@gmail.com>
2022-05-16 00:59:39 +0200

    Navigation Bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,36 +1,22 @@
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Fragment } from "react";
 
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>Cukiernia</div>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <NavLink to='/hello' activeClassName={classes.active}>
-              Strona główna
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/about' activeClassName={classes.active}>
-              O nas
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/shop' activeClassName={classes.active}>
-              Produkty
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/cart' activeClassName={classes.active}>
-              Koszyk
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <Fragment>
+      <header>
+        <div className={classes.nav}>
+          <ul>
+            <li><Link to='/hello'>Strona główna</Link></li>
+            <li><Link to='/about'>O nas</Link></li>
+            <li><Link to='/shop'>Produkty</Link></li>
+            <li><Link to='/cart'>Koszyk</Link></li>
+          </ul>
+        </div>
     </header>
+    </Fragment>
   );
 }
 
