3737443772e4966e92db2cdbbde6bb6dcb597088
Piotr Kałuziński <piotr.kaluzinski@student.wat.edu.pl>
2022-11-29 20:36:57 +0100

    build

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -1,3 +1,4 @@
-const API_URL = 'http://localhost:8000/api';
+//const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://webapp20221128161333.azurewebsites.net/api'
 
 export default API_URL;
